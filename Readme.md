538f694592dfcc5c0e06fba9346c3e909d104b9c
Alan Moraes <alanmoraex@gmail.com>
2020-06-09 12:09:08 -0300

    First commit project

---
+++ Readme.md
@@ -0,0 +1,3 @@
+Projeto casa criativa onde é um site de entretenimento entre os usuário com ideias.
+
+
---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+    function onoff() {
+      document
+      .querySelector("#modal")
+      .classList
+      .toggle("hide")
+    }
+    
+
+
+ 
